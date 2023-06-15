b3b91907ceb2ac639559b6e711fb42b42ac72634
Luis <luisdevv232@gmail.com>
2023-06-14 23:50:33 -0700

    Exercises 3, 4 and 5 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    const arrayCopy = [...array];
+    for (const element of elements) {
+        if (arrayCopy.indexOf(element) !== -1) {
+            arrayCopy.splice(arrayCopy.indexOf(element), 1);
+        }
+    }
+    return arrayCopy;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (typeof firstNumber !== "number" || typeof secondNumber !== "number") {
+        return "ERROR";
+    }
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+    let start;
+    let end;
+    if (firstNumber <= secondNumber) {
+        start = firstNumber;
+        end = secondNumber;
+    } else {
+        start = secondNumber;
+        end = firstNumber;
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
